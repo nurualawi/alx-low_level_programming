fa369e85c6853df535235da2363f90c84d0ca16f
nurualawi <nurualawi@yahoo.com>
2023-07-19 02:00:27 -0700

    1_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - in lower case
+ *
+ * Return: void 
+ */
+
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
