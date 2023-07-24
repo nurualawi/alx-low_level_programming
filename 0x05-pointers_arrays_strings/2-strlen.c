9f1c218cac21f8fe77de98596a99d4bc3bf8067b
nurualawi <nurualawi@yahoo.com>
2023-07-24 13:46:58 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * strlen - returns the lenght of a string
+  * @s string
+  * Return: lenght
+  */
+
+int strlen(char *s)
+
+{
+	int longi = 0;
+
+	while (*s != '\0*')
+
+	{
+	longi++;
+	s++;
+	}
+
+	return (longi);
+
+
+}
